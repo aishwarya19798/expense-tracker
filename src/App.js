e3b76da6dd152c87c1e0ef308184ee4ddf60a087
Aishwarya Sharma <46526203+aishwarya19798@users.noreply.github.com>
2022-02-17 17:46:08 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -4,59 +4,53 @@ import Balance from "./component/Balance";
 import CashFlow from "./component/CashFlow";
 import History from "./component/History";
 import TransactionList from "./component/TransactionList";
-import './index.css';
-import * as _ from 'lodash'; 
+import "./index.css";
+import * as _ from "lodash";
 import { method } from "lodash";
 
-
 function App() {
-  const[transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState([]);
 
-  useEffect(()=>{
-    fetchData();    
-  },[])
+  useEffect(() => {
+    // fetchData();
+  }, []);
 
-  const fetchData = async() => {
-    const fetchAllTransactions = await fetch('http://localhost:5000/transactions')
-    const res = await fetchAllTransactions.json()
-    setTransactions(res)
-  }    
-
+  // const fetchData = async() => {
+  //   const fetchAllTransactions = await fetch('http://localhost:5000/transactions')
+  //   const res = await fetchAllTransactions.json()
+  //   setTransactions(res)
+  // }
 
   const addTrans = async (trans) => {
-      const res = await fetch('http://localhost:5000/transactions',{
-       method : "POST",
-       body : JSON.stringify(trans),
-       headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      }
-    })
-    fetchData()
+    //   const res = await fetch('http://localhost:5000/transactions',{
+    //    method : "POST",
+    //    body : JSON.stringify(trans),
+    //    headers: {
+    //     "Content-type": "application/json; charset=UTF-8"
+    //   }
+    // })
+    // fetchData()
 
-    // const id = Math.floor(Math.random() * 1000) + 1
-    // const newTrans = {id, ...trans}
-    // setTransactions([...transactions,newTrans])
-  }
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTrans = { id, ...trans };
+    setTransactions([...transactions, newTrans]);
+  };
 
-  const deleteTrans = async(id) => {
-    await fetch(`http://localhost:5000/transactions/${id}`,{
-      method:'DELETE'
-    })
+  const deleteTrans = async (id) => {
+    // await fetch(`http://localhost:5000/transactions/${id}`,{
+    //   method:'DELETE'
+    // })
 
-    setTransactions(
-      transactions.filter(
-          (x) => x.id !== id
-        )
-      )
-  }
+    setTransactions(transactions.filter((x) => x.id !== id));
+  };
 
   return (
     <div className="App">
       <h2>Expense Tracker</h2>
-      <Balance totalAmount={ _.sumBy(transactions, x=>x.amount)} />
-      <CashFlow transactionList={transactions}/>
-      <History transactionList={transactions} onDelete={deleteTrans}/>
-      <AddTransaction addTrans={addTrans}/>
+      <Balance totalAmount={_.sumBy(transactions, (x) => x.amount)} />
+      <CashFlow transactionList={transactions} />
+      <History transactionList={transactions} onDelete={deleteTrans} />
+      <AddTransaction addTrans={addTrans} />
     </div>
   );
 }
